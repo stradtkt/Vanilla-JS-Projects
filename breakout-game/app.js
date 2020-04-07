d9aae92619e55929fb63dd0b0196f74317e9ef59
Kevin Stradtman <stradtkt22@gmail.com>
2020-04-07 17:28:23 -0400

    Added the moveball and lose game

--- breakout-game/app.js
+++ breakout-game/app.js
@@ -91,7 +91,52 @@ function movePaddle() {
 }
 
 function moveBall() {
-    
+    ball.x += ball.dx;
+    ball.y += ball.dy;
+    if(ball.x + ball.size > canvas.width || ball.x - ball.size < 0) {
+        ball.dx *= -1;
+    }
+
+    if(ball.y + ball.size > canvas.height || ball.y - ball.size < 0) {
+        ball.dy *= -1;
+    }
+
+    if(ball.x - ball.size > paddle.x && ball.x + ball.size < paddle.x + paddle.w && ball.y + ball.size > paddle.y) {
+        ball.dy = -ball.speed;
+    }
+
+    bricks.forEach(column => {
+        column.forEach(brick => {
+            if(brick.visible) {
+                if(ball.x - ball.size > brick.x && 
+                    ball.x + ball.size < brick.x + brick.w && 
+                    ball.y + ball.size > brick.y && 
+                    ball.y - ball.size < brick.y + brick.h) {
+                    ball.dy *= -1;
+                    brick.visible = false;
+                    increaseScore();
+                }
+            }
+        });
+    });
+
+    if(ball.y + ball.size > canvas.height) {
+        showAllBricks();
+        score = 0;
+    }
+}
+
+function increaseScore() {
+    score++;
+    if(score % (brickRowCount * brickRowCount) === 0) {
+        showAllBricks();
+    }
+}
+
+function showAllBricks() {
+    bricks.forEach(column => {
+        column.forEach(brick => brick.visible = true);
+    });
 }
 
 function draw() {
